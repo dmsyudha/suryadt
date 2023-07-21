3609a4376200b966bfa2f303ce2b0924c9796b8e
Dhimas Yudha Pratama <dhimas.yudha@mekari.com>
2023-07-22 03:03:40 +0700

    refactor(user controller): refactor validation code

--- services/UserService.js
+++ services/UserService.js
@@ -1,16 +1,32 @@
 const User = require('../models/User');
 const moment = require('moment');
 
-exports.createUser = async (userData) => {
-  // Validate using timezone, it automatically adjusts for daylight saving time
-  if (!moment.tz.zone(userData.time_zone)) {
+// Function to validate timezone
+const validateTimeZone = (timeZone) => {
+  if (!moment.tz.zone(timeZone)) {
     throw new Error('Invalid timezone');
   }
+}
 
-  // Validate unique email
-  if (!userData.email.includes('@')) {
+// Function to validate email
+const validateEmail = (email) => {
+  if (!email.includes('@')) {
     throw new Error('Invalid email');
   }
+}
+
+// Function to check if email is already in use
+const checkEmailInUse = async (email, id) => {
+  const existingUser = await User.findOne({ where: { email: email } });
+  if (existingUser && existingUser.id !== id) {
+    throw new Error('Email already in use');
+  }
+}
+
+exports.createUser = async (userData) => {
+  validateTimeZone(userData.time_zone);
+  validateEmail(userData.email);
+  await checkEmailInUse(userData.email);
 
   const user = await User.create(userData);
   return user;

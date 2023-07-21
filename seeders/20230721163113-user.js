2358ceadf0207d5fe0c2ea2e624a4b3c41abfcdd
Dhimas Yudha Pratama <dhimas.yudha@mekari.com>
2023-07-22 02:15:34 +0700

    feat: new seeder for testing

---
+++ seeders/20230721163113-user.js
@@ -0,0 +1,32 @@
+'use strict';
+const { faker } = require('@faker-js/faker');
+const moment = require('moment-timezone');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const timeZones = moment.tz.names();
+    const today = new Date();
+
+    let users = timeZones.map((timeZone) => {
+      const year = faker.number.int({min:1992, max:2000});
+      const birthday = new Date(year, today.getMonth(), today.getDate()); 
+
+      return {
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+        birthday: birthday,
+        location: faker.location.country(),
+        time_zone: timeZone,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    });
+
+    return queryInterface.bulkInsert('users', users, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('users', null, {});
+  }
+};

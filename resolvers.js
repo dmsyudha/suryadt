a3b7babb53cc63a1dd4531ab0d22857e1ec8764d
Dhimas Yudha Pratama <dmsyudha@live.com>
2023-08-07 04:45:01 +0700

    bug(graphql): update resolvers to handle offline mode

--- resolvers.js
+++ resolvers.js
@@ -8,10 +8,20 @@ module.exports = {
   },
   Mutation: {
     createUser: (_, { firstName, lastName, email, birthday, location }) => User.create({ firstName, lastName, email, birthday, location }),
-    updateUser: async (_, { id, firstName, lastName, email, birthday, location }) => {
+    updateUser: async (_, { id, firstName, lastName, email, birthday, location, lastKnownUpdatedAt }) => {
+      const existingUser = await User.findOne({ where: { id: id } });
+      if (!existingUser) {
+        throw new Error("User not found");
+      }
+    
+      // Check for conflicts
+      if (existingUser.updatedAt > new Date(lastKnownUpdatedAt)) {
+        throw new Error("CONFLICT");
+      }
+    
       await User.update({ firstName, lastName, email, birthday, location }, { where: { id } });
       return User.findOne({ where: { id } });
-    },
+    },    
     deleteUser: async (_, { id }) => {
       await User.destroy({ where: { id } });
       return true;
